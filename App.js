6215eaa976329179a422c91c0adf77491790e6bc
Luis Naranjo <jonaranj@microsoft.com>
2018-06-07 10:22:32 -0700

    finished sample app

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, AsyncStorage } from 'react-native';
+
+const STORAGE_KEY = "SAMPLE_ASYNC_STORAGE_ITEM";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: undefined
+    };
+    
+  }
+
+  componentDidMount() {
+    this.getItem();
+  }
+
+  getItem = () => {
+    AsyncStorage.getItem(STORAGE_KEY).then(value => this.setState({value}));
+  }
+
+  setItem = () => {
+    AsyncStorage.setItem(STORAGE_KEY, "HELLO").then(ignore => this.getItem());
+  }
+
+  removeItem = () => {
+    AsyncStorage.removeItem(STORAGE_KEY).then(ignore => this.getItem());
+  }
+
+  clear = () => {
+    AsyncStorage.clear().then(ignore => this.getItem());
+  }
+
+  getAllKeys = () => {
+    AsyncStorage.getAllKeys().then(keys => keys.forEach(key => console.warn(`Key: ${key}`)));
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Storage value: {this.state.value}</Text>
+        <Button title={"get item"} onPress={this.getItem}/>
+        <Button title={"set item"} onPress={this.setItem}/>
+        <Button title={"remove item"} onPress={this.removeItem}/>
+        <Button title={"clear"} onPress={this.clear}/>
+        <Button title={"get all keys"} onPress={this.getAllKeys}/>
       </View>
     );
   }
@@ -18,4 +57,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    
+  }
 });
